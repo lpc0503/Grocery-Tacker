4a23c0d51a72420d666ed638ef849a900955f61c
ICEJJ <lpc0503@gmail.com>
2024-06-23 17:15:57 -0400

    first commit

---
+++ graph/resolver.go
@@ -0,0 +1,10 @@
+package graph
+
+import "github.com/lpc0503/Grocery-Tacker/graph/model"
+
+//go:generate go run -mod=mod github.com/99designs/gqlgen generate --verbose
+
+type Resolver struct {
+	users        []*model.User
+	groceryItems []*model.GroceryItem
+}
